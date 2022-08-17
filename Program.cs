ec143a3ee242cd2601a494928db40f04af721878
souvikghosh007 <111341297+souvikghosh007@users.noreply.github.com>
2022-08-17 08:48:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mynum = 25;
+            float myfloat = 5.2f;
+            bool mybool = true;
+            Console.WriteLine(Convert.ToString(mynum));
+            Console.WriteLine(Convert.ToInt32(myfloat));
+            Console.WriteLine(Convert.ToDouble(myfloat));
+            Console.WriteLine(Convert.ToInt32(mybool));
+        }
+    }
+}
+
+
