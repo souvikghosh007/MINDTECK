d3cae2788eb3f319ad90e7a8117d5d552ac4304f
souvikghosh007 <111341297+souvikghosh007@users.noreply.github.com>
2022-08-19 01:18:17 +0530

    Add files via upload

---
+++ try catch method.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace myconsoleapplication
+{
+    class souvik
+    {
+        public delegate void Souvikdeligate(string name);
+        public delegate void Multideligate(int x, int y);
+        public void Multi(int x, int y)
+        {
+            Console.WriteLine(x * y);
+
+        }
+        public void name(string name)
+        {
+            Console.WriteLine("my name is" + " " + name);
+
+        }
+        static void Main(string[] args)
+        {
+            souvik d = new souvik();
+            Souvikdeligate obj1 = new Souvikdeligate(d.name);
+            Multideligate obj2 = new Multideligate(d.Multi);
+            obj1("Souvik");
+            obj2(10, 20);
+
+        }
+
+    }
+
+}
