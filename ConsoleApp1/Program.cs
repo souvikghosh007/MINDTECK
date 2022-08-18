e92c933ec2568b376967fbc3ed11b7b216965a9c
souvikghosh007 <111341297+souvikghosh007@users.noreply.github.com>
2022-08-19 01:05:53 +0530

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AccessModifiers
+{
+    class Program
+    {
+        class AccessMod
+        {
+            public int num1;
+            public int num2;
+        }
+        static void Main(string[] args)
+        {
+            AccessMod ob1 = new AccessMod();
+            //Direct access to public members  
+            ob1.num1 = 100;
+            //Access to private member is not permitted  
+            ob1.num2 = 20;
+            Console.WriteLine("Number one value in main {0}", ob1.num1);
+            Console.ReadLine();
+        }
+    }
+}
