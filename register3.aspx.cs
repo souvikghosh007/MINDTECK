ff119a3bdc4fd1ed2523ad9b24703caff7fae072
souvikghosh007 <111341297+souvikghosh007@users.noreply.github.com>
2022-08-29 10:08:13 +0530

    Add files via upload

---
+++ register3.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WebApplication1
+{
+    public partial class register3 : System.Web.UI.Page
+    {
+        SqlConnection SqlCon = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        string myConnectionString = ConfigurationManager.ConnectionStrings["StrCon"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void textName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void textMobilenumber_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCon.ConnectionString = (myConnectionString);
+                SqlCon.Open();
+                com.Connection = SqlCon;
+                com.CommandText = "INSERT INTO [dbo].[RegisterStudents] (Name,Mobile Number,EmailId) VALUES (@Name,@Mobile,@Email)";
+                com.Parameters.AddWithValue("@Name", textName.Text.Trim());
+                com.Parameters.AddWithValue("@Mobile", textMobilenumber.Text.Trim());
+                com.Parameters.AddWithValue("@Email", textMailId.Text.Trim());
+                com.ExecuteNonQuery();
+                SqlCon.Close();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
