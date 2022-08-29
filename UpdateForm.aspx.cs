7f85e7fc93baf5c58545bf62ee180e6086e95326
souvikghosh007 <111341297+souvikghosh007@users.noreply.github.com>
2022-08-29 10:58:07 +0530

    Add files via upload

---
+++ UpdateForm.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mysecondapp
+{
+    public partial class Regitration : System.Web.UI.Page
+    {
+        SqlConnection SqlCon = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        string myConnectionString = ConfigurationManager.ConnectionStrings["StrCon"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRegistrationDetails();
+
+            }
+
+        }
+        private void LoadRegistrationDetails()
+        {
+            try
+            {
+                SqlCon.ConnectionString = (myConnectionString);
+                SqlCon.Open();
+                com = new SqlCommand("GetRegistrationDetails", SqlCon);// passing stored proc name here 
+                com.CommandType = CommandType.StoredProcedure; // stored proc
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                grdDetails.DataSource = ds;
+                grdDetails.DataBind();
+                SqlCon.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+        }
+
+    }
+}
--- ViewSwitcher.ascx.cs
+++ ViewSwitcher.ascx.cs
@@ -7,7 +7,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.FriendlyUrls.Resolvers;
 
-namespace WebApplication2
+namespace WebApplication3
 {
     public partial class ViewSwitcher : System.Web.UI.UserControl
     {
